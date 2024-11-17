95ef5b0c4b6c94ffd6da031f99a6d49663b7f00b
Farhan-dev-dot <farhanhan2478@gmail.com>
2024-11-17 14:59:15 +0700

    Update Model_kategori.php

--- application/models/Model_kategori.php
+++ application/models/Model_kategori.php
@@ -14,8 +14,21 @@ class Model_kategori extends CI_Model
         $this->db->insert('kategori_produk', $data);
     }
 
-    public function getkategoriById($id)
+    public function getKategoriById($id)
     {
         return $this->db->get_where('kategori_produk', ['id_kategori' => $id])->row_array();
+    }
+
+
+    public function updateKategori($id_kategori, $data)
+    {
+        $this->db->where('id_kategori', $id_kategori);
+        return $this->db->update('kategori_produk', $data);
+    }
+
+    public function deletekategori($id_kategori)
+    {
+        $this->db->delete('kategori_produk', ['id_kategori' => $id_kategori]);
+        return $this->db->affected_rows();
     }
 }
